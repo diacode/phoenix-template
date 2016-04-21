9a58a1f99cefc2c6acc406dcfe65495b9e0e5430
Ricardo García Vega <bigardone@gmail.com>
2016-04-21 16:34:28 +0200

    Added new action route, view and template

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule PhoenixTemplate.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule PhoenixTemplate.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/new", PageController, :new
   end
 
   # Other scopes may use custom stacks.
