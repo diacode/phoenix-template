5c689b339495b5f671a0c7cb2462a29d3059abd7
Ricardo García Vega <bigardone@gmail.com>
2016-04-21 16:35:50 +0200

    Added function in view to set the expected js view class name

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,9 +1,13 @@
 defmodule PhoenixTemplate.LayoutView do
   use PhoenixTemplate.Web, :view
 
-  def js_view_path(conn) do
+  def js_view_name(conn) do
     view_name = Phoenix.Naming.resource_name(view_module(conn))
+    action_name = action_name(conn)
 
-    "#{view_name}/#{action_name(conn)}"
+    "#{view_name}_#{action_name}"
+    |> String.split("_")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join("")
   end
 end
