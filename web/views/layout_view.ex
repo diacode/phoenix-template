6fd137d71415082d5e664fdbcc2bc74975cee863
Ricardo García Vega <bigardone@gmail.com>
2016-04-21 12:17:29 +0200

    Added view helper function for generating the js view path depending on the current view and action name.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,9 @@
 defmodule PhoenixTemplate.LayoutView do
   use PhoenixTemplate.Web, :view
+
+  def js_view_path(conn) do
+    view_name = Phoenix.Naming.resource_name(view_module(conn))
+
+    "#{view_name}/#{action_name(conn)}"
+  end
 end
