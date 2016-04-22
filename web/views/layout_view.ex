d73c3140e746e5ce63b416cf811facca99f63973
Ricardo García Vega <bigardone@gmail.com>
2016-04-22 08:48:20 +0200

    Fixed js view function

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -2,19 +2,17 @@ defmodule PhoenixTemplate.LayoutView do
   use PhoenixTemplate.Web, :view
 
   def js_view_name(conn) do
-    view_name = Phoenix.Naming.resource_name(view_module(conn))
+    controller_name = Phoenix.Naming.resource_name(controller_module(conn))
     action_name = action_name(conn)
 
-    generate_name("#{action_name}_#{view_name}") <> "View"
+    generate_name("#{controller_name}_#{action_name}_view")
   end
 
   defp generate_name(value) do
     value
+    |> String.replace("_controller", "")
     |> String.split("_")
-    |> Enum.reverse
-    |> List.delete_at(0)
     |> Enum.map(&String.capitalize/1)
-    |> Enum.reverse
     |> Enum.join("")
   end
 end
